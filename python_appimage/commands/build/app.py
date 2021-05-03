874a28d82c403dd869b46c316eaa047027fb374a
Andy Kipp <anki-code@users.noreply.github.com>
2021-05-03 11:23:49 +0300

    Update app.py

--- python_appimage/commands/build/app.py
+++ python_appimage/commands/build/app.py
@@ -228,8 +228,9 @@ def execute(appdir, name=None, python_version=None, linux_tag=None,
 
         # Bundle the requirements
         if requirements_list:
+            in_tree_build = '' if version <= '3.5' else '--use-feature=in-tree-build'
             deprecation = 'DEPRECATION: Python 2.7 reached the end of its life'
-            system(('./AppDir/AppRun', '-m', 'pip', 'install', '-U', '--use-feature=in-tree-build',
+            system(('./AppDir/AppRun', '-m', 'pip', 'install', '-U', in_tree_build,
                    '--no-warn-script-location', 'pip'), exclude=deprecation)
             for requirement in requirements_list:
                 if requirement.startswith('git+'):
@@ -237,7 +238,7 @@ def execute(appdir, name=None, python_version=None, linux_tag=None,
                     log('BUNDLE', name + ' from ' + url[4:])
                 else:
                     log('BUNDLE', requirement)
-                system(('./AppDir/AppRun', '-m', 'pip', 'install', '-U', '--use-feature=in-tree-build',
+                system(('./AppDir/AppRun', '-m', 'pip', 'install', '-U', in_tree_build,
                        '--no-warn-script-location', requirement),
                        exclude=(deprecation, '  Running command git clone -q'))
 
