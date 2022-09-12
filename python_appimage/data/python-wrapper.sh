6dfa764573f63e73a8d4e9982ad3762e048a9b9b
Valentin Niess <niess@in2p3.fr>
2022-09-12 10:24:14 +0200

    Protect spaces (issue 55)

--- python_appimage/data/python-wrapper.sh
+++ python_appimage/data/python-wrapper.sh
@@ -2,16 +2,16 @@
 
 # If running from an extracted image, then export ARGV0 and APPDIR
 if [ -z "${APPIMAGE}" ]; then
-    export ARGV0=$0
+    export ARGV0="$0"
 
-    self="$(readlink -f -- $0)"
+    self=$(readlink -f -- "$0") # Protect spaces (issue 55)
     here="${self%/*}"
     tmp="${here%/*}"
     export APPDIR="${tmp%/*}"
 fi
 
 # Resolve the calling command (preserving symbolic links).
-export APPIMAGE_COMMAND="$(command -v -- $ARGV0)"
+export APPIMAGE_COMMAND=$(command -v -- "$ARGV0")
 {{ tcltk-env }}
 {{ cert-file }}
 
